ada9af84bcf1ff7957531abb1e7f31c2ded92bf9
joaolsvieira <joaolsvieira@gmail.com>
2018-01-24 18:41:38 -0500

    edit

--- SQL/Interviews.sql
+++ SQL/Interviews.sql
@@ -1,4 +1,5 @@
 --Interviews
+--Write a query to print the contest_id, hacker_id, name, and the sums of total_submissions, total_accepted_submissions, --total_views, and total_unique_views for each contest sorted by contest_id. Exclude the contest from the result if all four --sums are 0.
 
 --Contests (contest_id, hacker_id,name)
 --Colleges (college_id, contest_id)
